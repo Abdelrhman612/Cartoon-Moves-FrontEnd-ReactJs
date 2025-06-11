f4a782b1f4119d199461df3d3a5ef968756f5441
Abdelrhman <abdo612200320@gmail.com>
2025-06-11 16:47:39 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import SignUpCard from "./Pages/SignUp/SignUpCard";
 import { BrowserRouter } from "react-router";
 import "./index.css";
+import SignInCard from "./Pages/SignIn/SignInCard";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/signUp" element={<SignUpCard />} />
+          <Route path="/" element={<SignInCard />}></Route>
         </Routes>
       </BrowserRouter>
     </>
