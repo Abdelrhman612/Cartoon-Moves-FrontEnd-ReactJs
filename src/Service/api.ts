6654cfc0d6348f7df6ee5095e924efa69dbd7193
Abdelrhman <abdo612200320@gmail.com>
2025-06-17 14:34:00 +0300

    update api.ts

--- src/Service/api.ts
+++ src/Service/api.ts
@@ -2,3 +2,4 @@
 const api_Service = import.meta.env.VITE_API_SERVICE_URL;
 export const SignUpUrl = `${api_Service}/auth/sign-up`;
 export const SignInUrl = `${api_Service}/auth/sign-in`;
+export const MovieUrl = `${api_Service}/movie`;
