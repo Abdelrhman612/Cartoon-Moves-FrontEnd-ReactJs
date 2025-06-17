7d97caaed3958c79e55227df85b878c49d39d1b0
Abdelrhman <abdo612200320@gmail.com>
2025-06-17 14:33:23 +0300

    feat: create MovieCard.tsx

---
+++ src/Components/Movie/MovieCard.tsx
@@ -0,0 +1,19 @@
+import { Button, Card } from "react-bootstrap";
+import type { MovieCardProps } from "./MoveCard.interface";
+
+const MovieCard = ({ title, description, videoUrl, image }: MovieCardProps) => {
+  return (
+    <Card style={{ height: "100%" }}>
+      <Card.Img variant="top" src={image} height={300} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="success" href={videoUrl}>
+          Watch now
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default MovieCard;
