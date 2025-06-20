a94940fab0fd247104f91bcd6c0073e91ea034e2
Abdelrhman <abdo612200320@gmail.com>
2025-06-20 17:42:22 +0300

    update Move.interface.ts

--- src/Components/Movie/Move.interface.ts
+++ src/Components/Movie/Move.interface.ts
@@ -3,4 +3,29 @@ export interface MovieCardProps {
   description: string;
   videoUrl: string;
   image: string;
+  onEdit: () => void;
+}
+export interface MovieInterface {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  videoUrl: string;
+}
+
+export interface createMovie {
+  title: string;
+  description: string;
+  videoUrl: string;
+  image: string;
+}
+export interface PropsModal {
+  show: boolean;
+  onClose: () => void;
+  initialData?: MovieInterface;
+  onSubmit: () => void;
+}
+export interface PropsForm {
+  initialData?: MovieInterface;
+  onSubmit: () => void;
 }
